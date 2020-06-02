5b148a9ae992c382d10704373be8b01df027e148
Paulina Wróbel <paulina.wrobel@microsoft.wsei.edu.pl>
2020-06-02 23:42:10 +0200

    change

--- dist/App.js
+++
@@ -1,9 +0,0 @@
-import { newColumnManager } from "./src/newColumnManager";
-import { loadColumns } from "./src/loadColumnsFromStorage";
-// load columns from storage
-window.onload = function () {
-    loadColumns();
-};
-// initialization of 'column manager' to start whole interaction
-let columnManager = new newColumnManager("createNewColumnInput", "createNewColumnButton");
-//# sourceMappingURL=App.js.map
